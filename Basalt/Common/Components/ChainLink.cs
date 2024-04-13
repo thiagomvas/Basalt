70e6af19e617fd85650e30269b87c501508018f1
Thiago Menezes <thiagomvas@gmail.com>
2024-04-13 12:12:05 -0300

    Add Links

---
+++ Basalt/Common/Components/ChainLink.cs
@@ -0,0 +1,48 @@
+﻿using Basalt.Common.Entities;
+using System.Numerics;
+
+namespace Basalt.Common.Components
+{
+	public class ChainLink : Component
+	{
+		public Entity AnchoredEntity { get; set; }
+		public float MaxDistance { get; set; }
+		public ChainLink(Entity entity) : base(entity)
+		{
+		}
+
+		public override void OnStart()
+		{
+
+		}
+
+		public override void OnUpdate()
+		{
+			Vector3 dir = AnchoredEntity.Transform.Position - Entity.Transform.Position;
+
+			float distance = dir.LengthSquared();
+
+			if (distance > MaxDistance * MaxDistance)
+			{
+				Vector3 displacement = Vector3.Normalize(dir) * (MathF.Sqrt(distance) - MaxDistance);
+
+				Entity.Transform.Position += displacement * 0.5f;
+				AnchoredEntity.Transform.Position -= displacement * 0.5f;
+
+				if (Entity.Rigidbody != null && AnchoredEntity.Rigidbody != null)
+				{
+					Vector3 relativeVelocity = Entity.Rigidbody.Velocity - AnchoredEntity.Rigidbody.Velocity;
+					float relativeSpeed = Vector3.Dot(relativeVelocity, displacement);
+
+					if (relativeSpeed < 0)
+					{
+						Vector3 correctionVelocity = -relativeSpeed * displacement;
+
+						Entity.Rigidbody.Velocity += correctionVelocity * 0.5f;
+						AnchoredEntity.Rigidbody.Velocity -= correctionVelocity * 0.5f;
+					}
+				}
+			}
+		}
+	}
+}
---
+++ Basalt/Common/Components/FixedLink.cs
@@ -0,0 +1,48 @@
+﻿using Basalt.Common.Entities;
+using System.Numerics;
+
+namespace Basalt.Common.Components
+{
+	public class FixedLink : Component
+	{
+		public Entity AnchoredEntity { get; set; }
+		public float Distance { get; set; }
+		public FixedLink(Entity entity) : base(entity)
+		{
+		}
+
+		public override void OnStart()
+		{
+
+		}
+
+		public override void OnUpdate()
+		{
+			Vector3 dir = AnchoredEntity.Transform.Position - Entity.Transform.Position;
+
+			var currentDistance = dir.Length();
+
+			Vector3 displacement = Vector3.Normalize(dir) * (currentDistance - Distance);
+
+			Entity.Transform.Position += displacement * 0.5f;
+			AnchoredEntity.Transform.Position -= displacement * 0.5f;
+
+			// Check if the entities have Rigidbody components
+			Rigidbody? entityRigidbody = Entity.Rigidbody;
+			Rigidbody? anchoredEntityRigidbody = AnchoredEntity.Rigidbody;
+
+			if (entityRigidbody != null && anchoredEntityRigidbody != null)
+			{
+				// Calculate the total mass of the entities
+				float totalMass = entityRigidbody.Mass + anchoredEntityRigidbody.Mass;
+
+				// Calculate the velocity adjustment based on the mass ratio
+				float velocityAdjustment = displacement.Length() * (entityRigidbody.Mass / totalMass);
+
+				// Remove velocity from the entities
+				entityRigidbody.Velocity += displacement * (entityRigidbody.Mass / totalMass);
+				anchoredEntityRigidbody.Velocity -= displacement * (anchoredEntityRigidbody.Mass / totalMass);
+			}
+		}
+	}
+}
