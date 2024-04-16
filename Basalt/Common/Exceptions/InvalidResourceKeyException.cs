c52c54140c4b67b0c5367abcc3147a79e4928c60
Thiago Menezes <thiagomvas@gmail.com>
2024-04-16 13:06:30 -0300

    Add InvalidResourceKeyException

---
+++ Basalt/Common/Exceptions/InvalidResourceKeyException.cs
@@ -0,0 +1,9 @@
+﻿namespace Basalt.Common.Exceptions
+{
+	public class InvalidResourceKeyException : Exception
+	{
+		public InvalidResourceKeyException(string key) : base($"The resource key '{key}' is invalid or not found. Make sure the key is correctly typed or whether the resource was loaded or not.")
+		{
+		}
+	}
+}
