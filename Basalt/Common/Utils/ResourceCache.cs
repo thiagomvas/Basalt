16aa5bacaaa534c9e20c6c7e8c4903bfbd517a10
Thiago Menezes <thiagomvas@gmail.com>
2024-05-23 12:17:06 -0300

    feat: Reworked ResourceCache to store objects instead of strings

--- Basalt/Common/Utils/ResourceCache.cs
+++ Basalt/Common/Utils/ResourceCache.cs
@@ -7,11 +7,12 @@ namespace Basalt.Common.Utils
 	public class ResourceCache
 	{
 		private static ResourceCache instance;
-		private Dictionary<string, string> resourceCache;
+		private static readonly object padlock = new object();
+		private Dictionary<string, object> resourceCache;
 
 		private ResourceCache()
 		{
-			resourceCache = new Dictionary<string, string>();
+			resourceCache = new Dictionary<string, object>();
 		}
 
 		/// <summary>
@@ -30,49 +31,157 @@ namespace Basalt.Common.Utils
 		}
 
 		/// <summary>
-		/// Gets the resource content as a string for the specified resource name.
+		/// Caches a resource with the specified name.
 		/// </summary>
 		/// <param name="resourceName">The name of the resource.</param>
-		/// <returns>The resource content as a string.</returns>
-		public static string GetResourceString(string resourceName)
+		/// <param name="resource">The resource to cache.</param>
+		public static void CacheResource(string resourceName, object resource)
 		{
-			return Instance.GetResource(resourceName);
+			lock (padlock)
+			{
+				if (!Instance.resourceCache.ContainsKey(resourceName))
+				{
+					Instance.resourceCache.Add(resourceName, resource);
+				}
+			}
 		}
 
 		/// <summary>
-		/// Gets the resource content as an object of type T for the specified resource name.
+		/// Gets a resource with the specified name.
 		/// </summary>
-		/// <typeparam name="T">The type of the resource object.</typeparam>
+		/// <typeparam name="T">The type of the resource.</typeparam>
 		/// <param name="resourceName">The name of the resource.</param>
-		/// <returns>The resource content as an object of type T.</returns>
-		public static T GetResourceAs<T>(string resourceName)
+		/// <returns>The resource with the specified name, or the default value if not found.</returns>
+		public static T GetResource<T>(string resourceName)
 		{
-			return Instance.GetResource<T>(resourceName);
-		}
-
-		private string GetResource(string resourceName)
-		{
-			if (resourceCache.ContainsKey(resourceName))
+			lock (padlock)
 			{
-				return resourceCache[resourceName];
-			}
-			else
-			{
-				string resourceContent = LoadResourceFromFile(resourceName);
-				resourceCache.Add(resourceName, resourceContent);
-				return resourceContent;
+				if (Instance.resourceCache.ContainsKey(resourceName))
+				{
+					return (T)Instance.resourceCache[resourceName];
+				}
+				else
+				{
+					return default;
+				}
 			}
 		}
 
-		private T GetResource<T>(string resourceName)
+		/// <summary>
+		/// Tries to get a resource with the specified name.
+		/// </summary>
+		/// <typeparam name="T">The type of the resource.</typeparam>
+		/// <param name="resourceName">The name of the resource.</param>
+		/// <param name="resource">When this method returns, contains the resource with the specified name, if found; otherwise, the default value.</param>
+		/// <returns>true if the resource with the specified name is found; otherwise, false.</returns>
+		public static bool TryGetResource<T>(string resourceName, out T resource)
 		{
-			return JsonConvert.DeserializeAnonymousType<T>(GetResource(resourceName), default(T));
+			lock (padlock)
+			{
+				if (Instance.resourceCache.ContainsKey(resourceName))
+				{
+					resource = (T)Instance.resourceCache[resourceName];
+					return true;
+				}
+				else
+				{
+					resource = default;
+					return false;
+				}
+			}
 		}
 
-		private string LoadResourceFromFile(string resourceName)
+		/// <summary>
+		/// Loads a resource from a file and caches it with the specified name.
+		/// </summary>
+		/// <param name="resourceName">The name of the resource.</param>
+		/// <param name="filePath">The path to the file.</param>
+		public static void LoadResourceFromFile(string resourceName, string filePath)
 		{
-			string resourceContent = File.ReadAllText(resourceName);
-			return resourceContent;
+			lock (padlock)
+			{
+				if (File.Exists(filePath))
+				{
+					string json = File.ReadAllText(filePath);
+					object resource = JsonConvert.DeserializeObject(json);
+					Instance.resourceCache.Add(resourceName, resource);
+				}
+			}
+		}
+
+		/// <summary>
+		/// Loads a resource from a file using a custom load function and caches it with the specified name.
+		/// </summary>
+		/// <param name="resourceName">The name of the resource.</param>
+		/// <param name="filePath">The path to the file.</param>
+		/// <param name="loadFunc">The custom load function.</param>
+		public static void LoadResourceFromFile(string resourceName, string filePath, Func<string, object> loadFunc)
+		{
+			lock (padlock)
+			{
+				if (File.Exists(filePath))
+				{
+					object resource = loadFunc(filePath);
+					Instance.resourceCache.Add(resourceName, resource);
+				}
+			}
+		}
+
+		/// <summary>
+		/// Saves a resource with the specified name to a file.
+		/// </summary>
+		/// <param name="resourceName">The name of the resource.</param>
+		/// <param name="filePath">The path to the file.</param>
+		public static void SaveResourceToFile(string resourceName, string filePath)
+		{
+			lock (padlock)
+			{
+				if (Instance.resourceCache.ContainsKey(resourceName))
+				{
+					object resource = Instance.resourceCache[resourceName];
+					string json = JsonConvert.SerializeObject(resource);
+					File.WriteAllText(filePath, json);
+				}
+			}
+		}
+
+		/// <summary>
+		/// Unloads a resource with the specified name from the cache.
+		/// </summary>
+		/// <param name="resourceName">The name of the resource.</param>
+		public static void UnloadResource(string resourceName)
+		{
+			lock (padlock)
+			{
+				if (Instance.resourceCache.ContainsKey(resourceName))
+				{
+					Instance.resourceCache.Remove(resourceName);
+				}
+			}
+		}
+
+		/// <summary>
+		/// Checks if the cache contains a resource with the specified name.
+		/// </summary>
+		/// <param name="resourceName">The key of the resource expected to be loaded</param>
+		/// <returns>True if there is a loaded resource with that key, false otherwise</returns>
+		public static bool HasResourceKey(string resourceName)
+		{
+			lock (padlock)
+			{
+				return Instance.resourceCache.ContainsKey(resourceName);
+			}
+		}
+
+		/// <summary>
+		/// Clears the resource cache.
+		/// </summary>
+		public static void Clear()
+		{
+			lock (padlock)
+			{
+				Instance.resourceCache.Clear();
+			}
 		}
 	}
 }
