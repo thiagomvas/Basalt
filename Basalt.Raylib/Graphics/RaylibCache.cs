b5f105da13b7034eaee618b5d6246536a658a6e9
Thiago Menezes <thiagomvas@gmail.com>
2024-05-23 12:17:33 -0300

    feat: RaylibCache reworked to be extensions method of ResourceCache instead

--- Basalt.Raylib/Graphics/RaylibCache.cs
+++ Basalt.Raylib/Graphics/RaylibCache.cs
@@ -1,58 +1,44 @@
-﻿using Raylib_cs;
+﻿using Basalt.Common.Utils;
+using Raylib_cs;
+using System.Net.NetworkInformation;
 
 namespace Basalt.Raylib.Graphics
 {
-	/// <summary>
-	/// Represents a cache for Raylib models and shaders.
-	/// </summary>
-	public unsafe class RaylibCache
+	public unsafe static class ResourceCacheExtensions
 	{
-		/// <summary>
-		/// The singleton instance of the RaylibCache class.
-		/// </summary>
-		private static RaylibCache instance;
-
-		/// <summary>
-		/// The dictionary that stores the cached models.
-		/// </summary>
-		private Dictionary<string, Model> modelDictionary = new();
-
-		/// <summary>
-		/// The dictionary that stores the cached shaders.
-		/// </summary>
-		private Dictionary<string, Shader> shaderDictionary = new();
-
-		/// <summary>
-		/// The queue that stores the model load requests.
-		/// </summary>
-		private List<KeyValuePair<string, ModelLoadRequest>> modelLoadQueue = new();
-
-		/// <summary>
-		/// The queue that stores the shader load requests.
-		/// </summary>
-		private List<KeyValuePair<string, ShaderLoadRequest>> shaderLoadQueue = new();
 
 		/// <summary>
 		/// The lock object used for thread synchronization when accessing the model dictionary.
 		/// </summary>
-		private object modelLock = new object();
+		private static object modelLock = new object();
 
 		/// <summary>
 		/// The lock object used for thread synchronization when accessing the shader dictionary.
 		/// </summary>
-		private object shaderLock = new object();
+		private static object shaderLock = new object();
+		/// <summary>
+		/// The queue that stores the model load requests.
+		/// </summary>
+		private static List<KeyValuePair<string, ModelLoadRequest>> modelLoadQueue = new();
+
+		/// <summary>
+		/// The queue that stores the shader load requests.
+		/// </summary>
+		private static List<KeyValuePair<string, ShaderLoadRequest>> shaderLoadQueue = new();
+
+		private static List<string> modelKeys = new();
+		private static List<string> shaderKeys = new();
 
 		/// <summary>
 		/// Loads the models and shaders from the load queues.
 		/// </summary>
-		internal void LoadQueued()
+		internal static void LoadQueued(this ResourceCache cache)
 		{
 			lock (shaderLock)
 			{
 				foreach (var req in shaderLoadQueue)
 				{
-					Shader s = Raylib_cs.Raylib.LoadShader(req.Value.vertexShaderPath, req.Value.fragmentShaderPath);
-					shaderDictionary.Add(req.Key, s);
+					loadShader(req.Value);
 				}
 				shaderLoadQueue.Clear();
 			}
@@ -61,107 +47,9 @@ namespace Basalt.Raylib.Graphics
 			{
 				foreach (var req in modelLoadQueue)
 				{
-					Model m = Raylib_cs.Raylib.LoadModel(req.Value.modelPath);
-					if (!string.IsNullOrWhiteSpace(req.Value.shaderCacheKey))
-					{
-						for (int i = 0; i < m.MaterialCount; i++)
-						{
-							m.Materials[i].Shader = shaderDictionary[req.Value.shaderCacheKey];
-						}
-					}
-
-					modelDictionary.Add(req.Key, m);
+					loadModel(req.Value);
 				}
 				modelLoadQueue.Clear();
-			}
-		}
-
-		/// <summary>
-		/// Private constructor to prevent direct instantiation of the RaylibCache class.
-		/// </summary>
-		private RaylibCache()
-		{
-		}
-
-		/// <summary>
-		/// Gets the singleton instance of the RaylibCache class.
-		/// </summary>
-		public static RaylibCache Instance
-		{
-			get
-			{
-				if (instance == null)
-				{
-					instance = new RaylibCache();
-				}
-				return instance;
-			}
-		}
-
-		/// <summary>
-		/// Gets the cached model with the specified name.
-		/// </summary>
-		/// <param name="modelName">The name of the model.</param>
-		/// <returns>The cached model, or null if the model is not found.</returns>
-		public Model? GetModel(string modelName)
-		{
-			lock (modelLock)
-			{
-				if (modelDictionary.ContainsKey(modelName))
-				{
-					return modelDictionary[modelName];
-				}
-				else
-				{
-					return null;
-				}
-			}
-		}
-
-		/// <summary>
-		/// Caches the specified model with the given name.
-		/// </summary>
-		/// <param name="modelName">The name of the model.</param>
-		/// <param name="model">The model to cache.</param>
-		public void CacheModel(string modelName, Model model)
-		{
-			lock (modelLock)
-			{
-				if (!modelDictionary.ContainsKey(modelName))
-				{
-					modelDictionary.Add(modelName, model);
-				}
-			}
-		}
-
-		/// <summary>
-		/// Unloads all the cached models.
-		/// </summary>
-		public void UnloadAllModels()
-		{
-			lock (modelLock)
-			{
-				foreach (var model in modelDictionary.Values)
-				{
-					Raylib_cs.Raylib.UnloadModel(model);
-				}
-				modelDictionary.Clear();
-			}
-		}
-
-		/// <summary>
-		/// Unloads the cached model with the specified name.
-		/// </summary>
-		/// <param name="modelName">The name of the model to unload.</param>
-		public void UnloadModel(string modelName)
-		{
-			lock (modelLock)
-			{
-				if (modelDictionary.ContainsKey(modelName))
-				{
-					Raylib_cs.Raylib.UnloadModel(modelDictionary[modelName]);
-					modelDictionary.Remove(modelName);
-				}
 			}
 		}
 
@@ -171,7 +59,7 @@ namespace Basalt.Raylib.Graphics
 		/// <param name="modelName">The name of the model.</param>
 		/// <param name="modelPath">The path to the model file.</param>
 		/// <param name="shaderCacheKey">The optional shader cache key.</param>
-		public void LoadModel(string modelName, string modelPath, string shaderCacheKey = "")
+		public static void LoadModel(this ResourceCache cache, string modelName, string modelPath, string shaderCacheKey = "")
 		{
 			if (!Raylib_cs.Raylib.IsWindowReady())
 			{
@@ -193,86 +81,17 @@ namespace Basalt.Raylib.Graphics
 
 			lock (modelLock)
 			{
-				modelDictionary.Add(modelName, model);
+				loadModel(new(modelName, modelPath, shaderCacheKey));
 			}
 		}
-
 		/// <summary>
-		/// Gets the cached shader with the specified name.
+		/// Loads a shader from the specified fragment and vertex shader paths and caches it.
 		/// </summary>
-		/// <param name="shaderName">The name of the shader.</param>
-		/// <returns>The cached shader, or null if the shader is not found.</returns>
-		public Shader? GetShader(string shaderName)
-		{
-			lock (shaderLock)
-			{
-				if (shaderDictionary.ContainsKey(shaderName))
-				{
-					return shaderDictionary[shaderName];
-				}
-				else
-				{
-					return null;
-				}
-			}
-		}
-
-		/// <summary>
-		/// Caches the specified shader with the given name.
-		/// </summary>
-		/// <param name="shaderName">The name of the shader.</param>
-		/// <param name="shader">The shader to cache.</param>
-		public void CacheShader(string shaderName, Shader shader)
-		{
-			lock (shaderLock)
-			{
-				if (!shaderDictionary.ContainsKey(shaderName))
-				{
-					shaderDictionary.Add(shaderName, shader);
-					return;
-				}
-				shaderDictionary[shaderName] = shader;
-			}
-		}
-
-		/// <summary>
-		/// Unloads all the cached shaders.
-		/// </summary>
-		public void UnloadAllShaders()
-		{
-			lock (shaderLock)
-			{
-				foreach (var shader in shaderDictionary.Values)
-				{
-					Raylib_cs.Raylib.UnloadShader(shader);
-				}
-				shaderDictionary.Clear();
-			}
-		}
-
-		/// <summary>
-		/// Unloads the cached shader with the specified name.
-		/// </summary>
-		/// <param name="shaderName">The name of the shader to unload.</param>
-		public void UnloadShader(string shaderName)
-		{
-			lock (shaderLock)
-			{
-				if (shaderDictionary.ContainsKey(shaderName))
-				{
-					Raylib_cs.Raylib.UnloadShader(shaderDictionary[shaderName]);
-					shaderDictionary.Remove(shaderName);
-				}
-			}
-		}
-
-		/// <summary>
-		/// Loads a shader with the specified name, fragment shader path, and vertex shader path.
-		/// </summary>
-		/// <param name="shaderName">The name of the shader.</param>
+		/// <param name="cache">The resource cache instance.</param>
+		/// <param name="shaderName">The name of the shader to load.</param>
 		/// <param name="fragmentShaderPath">The path to the fragment shader file.</param>
 		/// <param name="vertexShaderPath">The path to the vertex shader file.</param>
-		public void LoadShader(string shaderName, string fragmentShaderPath, string vertexShaderPath)
+		public static void LoadShader(this ResourceCache cache, string shaderName, string fragmentShaderPath, string vertexShaderPath)
 		{
 			if (!Raylib_cs.Raylib.IsWindowReady())
 			{
@@ -293,35 +112,166 @@ namespace Basalt.Raylib.Graphics
 
 			lock (shaderLock)
 			{
-				shaderDictionary.Add(shaderName, shader);
+				ResourceCache.CacheResource(shaderName, shader);
 			}
 		}
 
 		/// <summary>
-		/// Checks if the model dictionary contains the specified key.
+		/// Retrieves a cached model by name.
 		/// </summary>
-		/// <param name="key">The key to check.</param>
-		/// <returns>True if the model dictionary contains the key, false otherwise.</returns>
-		public bool HasModelKey(string key)
+		/// <param name="cache">The resource cache instance.</param>
+		/// <param name="modelName">The name of the model to retrieve.</param>
+		/// <returns>The cached model if found; otherwise, null.</returns>
+		public static Model? GetModel(this ResourceCache cache, string modelName)
 		{
 			lock (modelLock)
 			{
-				return modelDictionary.ContainsKey(key);
+				if (ResourceCache.GetResource<Model>(modelName) is Model model)
+				{
+					return model;
+				}
+				else
+				{
+					return null;
+				}
 			}
 		}
 
 		/// <summary>
-		/// Checks if the shader dictionary contains the specified key.
+		/// Retrieves a cached shader by name.
 		/// </summary>
-		/// <param name="key">The key to check.</param>
-		/// <returns>True if the shader dictionary contains the key, false otherwise.</returns>
-		public bool HasShaderKey(string key)
+		/// <param name="cache">The resource cache instance.</param>
+		/// <param name="shaderName">The name of the shader to retrieve.</param>
+		/// <returns>The cached shader if found; otherwise, null.</returns>
+		public static Shader? GetShader(this ResourceCache cache, string shaderName)
 		{
 			lock (shaderLock)
 			{
-				return shaderDictionary.ContainsKey(key);
+				if (ResourceCache.GetResource<Shader>(shaderName) is Shader shader)
+				{
+					return shader;
+				}
+				else
+				{
+					return null;
+				}
 			}
 		}
+
+		/// <summary>
+		/// Caches a model with the specified name.
+		/// </summary>
+		/// <param name="cache">The resource cache instance.</param>
+		/// <param name="modelName">The name of the model to cache.</param>
+		/// <param name="model">The model to cache.</param>
+		public static void CacheModel(this ResourceCache cache, string modelName, Model model)
+		{
+			lock (modelLock)
+			{
+				ResourceCache.CacheResource(modelName, model);
+			}
+		}
+
+		/// <summary>
+		/// Caches a shader with the specified name.
+		/// </summary>
+		/// <param name="cache">The resource cache instance.</param>
+		/// <param name="shaderName">The name of the shader to cache.</param>
+		/// <param name="shader">The shader to cache.</param>
+		public static void CacheShader(this ResourceCache cache, string shaderName, Shader shader)
+		{
+			lock (shaderLock)
+			{
+				ResourceCache.CacheResource(shaderName, shader);
+			}
+		}
+
+		/// <summary>
+		/// Loads a model based on the specified request.
+		/// </summary>
+		/// <param name="req">The model load request containing model details.</param>
+		private static void loadModel(ModelLoadRequest req)
+		{
+			Model m = Raylib_cs.Raylib.LoadModel(req.modelPath);
+			if (!string.IsNullOrWhiteSpace(req.shaderCacheKey))
+			{
+				for (int i = 0; i < m.MaterialCount; i++)
+				{
+					m.Materials[i].Shader = ResourceCache.GetResource<Shader>(req.shaderCacheKey);
+				}
+			}
+
+			ResourceCache.CacheResource(req.modelName, m);
+
+			modelKeys.Add(req.modelName);
+		}
+
+		/// <summary>
+		/// Loads a shader based on the specified request.
+		/// </summary>
+		/// <param name="req">The shader load request containing shader details.</param>
+		private static void loadShader(ShaderLoadRequest req)
+		{
+			Shader s = Raylib_cs.Raylib.LoadShader(req.vertexShaderPath, req.fragmentShaderPath);
+			ResourceCache.CacheResource(req.shaderName, s);
+
+			shaderKeys.Add(req.shaderName);
+		}
+
+		/// <summary>
+		/// Unloads all cached Raylib models and shaders.
+		/// </summary>
+		/// <param name="cache">The resource cache instance.</param>
+		public static void UnloadRaylib(this ResourceCache cache)
+		{
+			lock (modelLock)
+			{
+				foreach (var key in modelKeys)
+				{
+					Raylib_cs.Raylib.UnloadModel(ResourceCache.GetResource<Model>(key));
+				}
+				modelKeys.Clear();
+			}
+
+			lock (shaderLock)
+			{
+				foreach (var key in shaderKeys)
+				{
+					Raylib_cs.Raylib.UnloadShader(ResourceCache.GetResource<Shader>(key));
+				}
+				shaderKeys.Clear();
+			}
+		}
+
+		internal static void LoadRaylibPrimitives(this ResourceCache cache)
+		{
+			// Models
+			Model cube = Raylib_cs.Raylib.LoadModelFromMesh(Raylib_cs.Raylib.GenMeshCube(1, 1, 1));
+			Model sphere = Raylib_cs.Raylib.LoadModelFromMesh(Raylib_cs.Raylib.GenMeshSphere(1, 16, 16));
+			Model plane = Raylib_cs.Raylib.LoadModelFromMesh(Raylib_cs.Raylib.GenMeshPlane(1, 1, 1, 1));
+			Model cylinder = Raylib_cs.Raylib.LoadModelFromMesh(Raylib_cs.Raylib.GenMeshCylinder(1, 1, 16));
+			Model torus = Raylib_cs.Raylib.LoadModelFromMesh(Raylib_cs.Raylib.GenMeshTorus(0.25f, 1, 16, 16));
+			Model knot = Raylib_cs.Raylib.LoadModelFromMesh(Raylib_cs.Raylib.GenMeshKnot(1, 1, 16, 128));
+
+			// Add shader if found
+			if(ResourceCache.TryGetResource("basalt.shaders.defaultlight", out Shader shader))
+			{
+				cube.Materials[0].Shader = shader;
+				sphere.Materials[0].Shader = shader;
+				plane.Materials[0].Shader = shader;
+				cylinder.Materials[0].Shader = shader;
+				torus.Materials[0].Shader = shader;
+				knot.Materials[0].Shader = shader;
+			}
+
+			ResourceCache.CacheResource("cube", cube);
+			ResourceCache.CacheResource("sphere", sphere);
+			ResourceCache.CacheResource("plane", plane);
+			ResourceCache.CacheResource("cylinder", cylinder);
+			ResourceCache.CacheResource("torus", torus);
+			ResourceCache.CacheResource("knot", knot);
+		}
+
 
 		/// <summary>
 		/// Represents a model load request.
@@ -331,6 +281,13 @@ namespace Basalt.Raylib.Graphics
 			public string modelName;
 			public string modelPath;
 			public string shaderCacheKey;
+
+			public ModelLoadRequest(string modelName, string modelPath, string shaderCacheKey)
+			{
+				this.modelName = modelName;
+				this.modelPath = modelPath;
+				this.shaderCacheKey = shaderCacheKey;
+			}
 		}
 
 		/// <summary>
@@ -341,6 +298,13 @@ namespace Basalt.Raylib.Graphics
 			public string shaderName;
 			public string fragmentShaderPath;
 			public string vertexShaderPath;
+
+			public ShaderLoadRequest(string shaderName, string fragmentShaderPath, string vertexShaderPath)
+			{
+				this.shaderName = shaderName;
+				this.fragmentShaderPath = fragmentShaderPath;
+				this.vertexShaderPath = vertexShaderPath;
+			}
 		}
 	}
 }
