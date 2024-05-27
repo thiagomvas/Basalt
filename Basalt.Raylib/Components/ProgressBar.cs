d1e34dc49cec92d2524ac909cdf2719cb580dd23
Thiago Menezes <thiagomvas@gmail.com>
2024-05-27 20:25:03 -0300

    Add progress bar

---
+++ Basalt.Raylib/Components/ProgressBar.cs
@@ -0,0 +1,37 @@
+﻿using Basalt.Common.Components;
+using Basalt.Common.Entities;
+using Raylib_cs;
+using System.Numerics;
+using static Raylib_cs.Raylib;
+
+namespace Basalt.Raylib.Components
+{
+	public class ProgressBar : UIComponent
+	{
+		public Vector2 Size { get; set; } = Vector2.One;
+		public Color BackgroundColor { get; set; } = Color.White;
+		public Color ForegroundColor { get; set; } = Color.Green;
+		public float Progress { get; set; } = 0.5f;
+		public ProgressBar(Entity entity) : base(entity)
+		{
+		}
+
+		public override void OnStart()
+		{
+
+		}
+
+		public override void OnUpdate()
+		{
+
+		}
+
+		public override void OnUIRender()
+		{
+			var position = GetPivotedPosition(new(GetScreenWidth(), GetScreenHeight())) + Offset;
+			DrawRectanglePro(new Rectangle(position.X - Size.X / 2, position.Y - Size.Y / 2, Size.X, Size.Y), Size / 2, Rotation, BackgroundColor);
+			DrawRectanglePro(new Rectangle(position.X - Size.X / 2, position.Y - Size.Y / 2, Size.X * Progress, Size.Y), Size / 2, Rotation, ForegroundColor);
+
+		}
+	}
+}
