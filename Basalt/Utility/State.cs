cc3ce612678e663ec46aced15a71decbb4dac3fb
Thiago Menezes <thiagomvas@gmail.com>
2024-06-09 16:52:38 -0300

    feat: Add generic state machine and state

---
+++ Basalt/Utility/State.cs
@@ -0,0 +1,35 @@
+﻿namespace Basalt.Utility
+{
+	/// <summary>
+	/// Represents an abstract state.
+	/// </summary>
+	/// <typeparam name="T">The type of the owner.</typeparam>
+	public abstract class State<T>
+	{
+		protected T Owner;
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="State{T}"/> class.
+		/// </summary>
+		/// <param name="Owner">The owner of the state.</param>
+		protected State(T Owner)
+		{
+			this.Owner = Owner;
+		}
+
+		/// <summary>
+		/// Called when entering the state.
+		/// </summary>
+		public virtual void Enter() { }
+
+		/// <summary>
+		/// Called when updating the state.
+		/// </summary>
+		public virtual void Update() { }
+
+		/// <summary>
+		/// Called when exiting the state.
+		/// </summary>
+		public virtual void Exit() { }
+	}
+}
---
+++ Basalt/Utility/StateMachine.cs
@@ -0,0 +1,76 @@
+﻿namespace Basalt.Utility
+{
+	/// <summary>
+	/// Represents a state machine that manages the state transitions for a given type.
+	/// </summary>
+	/// <typeparam name="T">The type of the owner of the state machine.</typeparam>
+	public class StateMachine<T>
+	{
+		private readonly T owner;
+		/// <summary>
+		/// Gets the current state of the state machine.
+		/// </summary>
+		/// <remarks>
+		/// The first state added to the state machine is set as the current state by default.
+		/// If no states have been added, this property will return <see langword="null"/>.
+		/// </remarks>
+		public State<T>? CurrentState { get; private set; }
+		private readonly Dictionary<Type, State<T>> states;
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="StateMachine{T}"/> class.
+		/// </summary>
+		/// <param name="owner">The owner of the state machine.</param>
+		public StateMachine(T owner)
+		{
+			this.owner = owner;
+			states = new Dictionary<Type, State<T>>();
+		}
+
+		/// <summary>
+		/// Adds a state to the state machine.
+		/// </summary>
+		/// <param name="state">The state to add.</param>
+		public void AddState(State<T> state)
+		{
+			states.TryAdd(state.GetType(), state);
+			CurrentState ??= state;
+		}
+
+		/// <summary>
+		/// Changes the current state of the state machine to the specified state.
+		/// </summary>
+		/// <typeparam name="S">The type of the state to change to.</typeparam>
+		public void ChangeState<S>() where S : State<T>
+		{
+			CurrentState?.Exit();
+			CurrentState = states[typeof(S)];
+			CurrentState.Enter();
+		}
+
+		/// <summary>
+		/// Updates the current state of the state machine.
+		/// </summary>
+		public void Update()
+		{
+			CurrentState?.Update();
+		}
+
+		/// <summary>
+		/// Exits the current state of the state machine.
+		/// </summary>
+		public void Exit()
+		{
+			CurrentState?.Exit();
+		}
+
+		/// <summary>
+		/// Gets an array of all the states in the state machine.
+		/// </summary>
+		/// <returns>An array of <see cref="State{T}"/> objects.</returns>
+		public State<T>[] GetStates()
+		{
+			return states.Values.ToArray();
+		}
+	}
+}
