0099ea85347604e6749e0a2e58fdd1aec30b1ba1
Thiago Menezes <thiagomvas@gmail.com>
2024-06-15 13:57:55 -0300

    feat: Add CameraControllerBase<T> generic class

---
+++ Basalt/Common/Components/CameraControllerBase.cs
@@ -0,0 +1,17 @@
+﻿using Basalt.Common.Entities;
+
+namespace Basalt.Common.Components
+{
+	public abstract class CameraControllerBase<T> : Component 
+	{
+		public abstract T Camera { get; set;  }
+		public CameraControllerBase(Entity entity) : base(entity)
+		{
+		}
+
+		public virtual void SetCamera(T camera)
+		{
+			Camera = camera;
+		}
+	}
+}
