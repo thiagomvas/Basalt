4298aa71e2297b4c629cea5516eb9f16147c4c38
Thiago Menezes <thiagomvas@gmail.com>
2024-06-15 13:17:21 -0300

    Add Particle.Scale and Particle.Color prroperties

--- Basalt/Types/Particle.cs
+++ Basalt/Types/Particle.cs
@@ -1,14 +1,32 @@
-﻿using System.Numerics;
+﻿using System.Drawing;
+using System.Numerics;
 
 namespace Basalt.Types
 {
 	public struct Particle
 	{
-		public Vector3 Position;
-		public Quaternion Rotation; 
-		public Vector3 Velocity;
-		public float Lifetime;
+		public Vector3 Position = Vector3.Zero;
+		public Quaternion Rotation = Quaternion.Identity; 
+		public Vector3 Velocity = Vector3.Zero;
+		public float Lifetime = 0;
+		public Vector3 Scale { get; set; } = Vector3.One;
+		public Color Color { get; set; } = Color.HotPink;
 
+        public Particle()
+        {
+			
+        }
+        public Particle(Vector3 position, Quaternion rotation)
+		{
+			Position = position;
+			Rotation = rotation;
+		}
+		public Particle(Vector3 position, Quaternion rotation, Vector3 velocity)
+		{
+			Position = position;
+			Rotation = rotation;
+			Velocity = velocity;
+		}
 		public Particle(Vector3 position, Quaternion rotation, Vector3 velocity, float lifetime)
 		{
 			Position = position;
@@ -16,5 +34,23 @@ namespace Basalt.Types
 			Velocity = velocity;
 			Lifetime = lifetime;
 		}
+		public Particle(Vector3 position, Quaternion rotation, Vector3 velocity, float lifetime, Vector3 scale)
+		{
+			Position = position;
+			Rotation = rotation;
+			Velocity = velocity;
+			Lifetime = lifetime;
+			Scale = scale;
+		}
+		public Particle(Vector3 position, Quaternion rotation, Vector3 velocity, float lifetime, Vector3 scale, Color color)
+		{
+			Position = position;
+			Rotation = rotation;
+			Velocity = velocity;
+			Lifetime = lifetime;
+			Scale = scale;
+			Color = color;
+		}
+
 	}
 }
